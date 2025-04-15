bdedf405454610bc58f23380de0a337fd88c8c2c
ilhyeonchoo <168050490+ilhyeonchoo@users.noreply.github.com>
2025-04-16 01:57:30 +0900

    week06 prac03,04 and skeleton

---
+++ week06/uni.py
@@ -0,0 +1,34 @@
+import math
+import statistics
+import struct
+import time
+import wave
+
+import pyaudio
+
+INTMAX = 2**(32-1)-1
+
+def send_data():
+    pass
+
+def receive_data():
+    pass
+
+def main():
+    while True:
+        print('Unicode over Sound With Noise')
+        print('2025 Spring Data Communication at CNU ')
+        print('[1] Send Unicode code over sound (play)')
+        print('[2] Receive Unicode code over sound (record)')
+        print('[q] Exit')
+        select = input('Select menu: ').strip().upper()
+        if select == '1':
+            send_data()
+        elif select == '2':
+            receive_data()
+        elif select == 'Q':
+            print('Terminating...')
+            break
+
+if __name__ == '__main__':
+    main()
